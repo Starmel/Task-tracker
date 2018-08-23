097007aca2c42166ccbad7d1d5ccee1b5df7533a
Starmel <slava.kornienko16@gmail.com>
2018-08-24 01:15:58 +0300

    designing Tasks List scene

--- Task-tracker/Src/App/Scene/FistTab/FirstViewController.swift
+++ Task-tracker/Src/App/Scene/FistTab/FirstViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+
+class FirstViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,19 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        presenter.specialSelected(at: indexPath.row)
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+//        presenter.setupItem(cell, indexPath.row)
+        return cell
+    }
 }
 
--- Task-tracker/Src/App/Scene/SecondTab/SecondViewController.swift
+++ Task-tracker/Src/App/Scene/SecondTab/SecondViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -19,7 +20,5 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
